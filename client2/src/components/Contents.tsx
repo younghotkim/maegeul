2ecf7e3ffd4678690e8973ed4dea5cdbc552f849
Yiyangse <azziyang@naver.com>
2024-09-04 17:04:36 +0900

    fix Footer 재배치

--- client2/src/components/Contents.tsx
+++ client2/src/components/Contents.tsx
@@ -5,13 +5,35 @@ import ImageSrc2 from '../Image/02.jpeg';
 import ImageSrc3 from '../Image/03.jpeg';
 import ImageSrc4 from '../Image/04.jpeg';
 import './Contents.css';
+// ArrowIcon ReactComponent로 불러옵니다.
+import { ReactComponent as ArrowIcon } from '../Icon/Arrow Right.svg';
 
 const Contents: React.FC = () => {
   const cardContents = [
-    { title: '자기 계발', text: '독서하기', tag: '#책추천', src: ImageSrc1 }, 
-    { title: '자기 관리', text: '일정한 시간에 잠자고 일어나기', tag: '#미라클모닝', src: ImageSrc2},
-    { title: '취미 활동', text: '맛있는 음식 먹기', tag: '#식단', src: ImageSrc3 }, 
-    { title: '건강', text: '스트레칭 하기', tag: '#명상', src: ImageSrc4 },
+    { 
+      title: '자기 계발', 
+      text: '더 단단한 나를 만드는 4가지 일상 실천 콘텐츠를 확인해보세요.', 
+      tag: ['독서하기','외국어 배우기','전공 공부하기','버킷리스트 작성하여 실천하기','봉사활동하기'], 
+      src: ImageSrc1 
+    }, 
+    { 
+      title: '자기 관리', 
+      text: '더 단단한 나를 만드는 4가지 일상 실천 콘텐츠를 확인해보세요.', 
+      tag: ['일정한 시간에 잠자고 일어나기', '감사일기 쓰기', '옷,방 정리하기','거울 보면서 웃기', '햇빛 쬐기', '명상·복식호흡하기','반신욕'], 
+      src: ImageSrc2 
+    },
+    { 
+      title: '취미 활동', 
+      text: '더 단단한 나를 만드는 4가지 일상 실천 콘텐츠를 확인해보세요.', 
+      tag: ['맛있는 음식 먹기','영화,드라마,예능보기', '그림그리기', '색칠하기', '식물키우기','여행가기','음악,라디오듣기','쇼핑하기'], 
+      src: ImageSrc3 
+    }, 
+    { 
+      title: '건강', 
+      text:  '더 단단한 나를 만드는 4가지 일상 실천 콘텐츠를 확인해보세요.', 
+      tag: ['스트레칭 하기', '운동하기', '댄스 춤추기','건강식품 섭취하기','체중조절하기'], 
+      src: ImageSrc4 
+    },
   ];
 
   return (
@@ -31,7 +53,7 @@ const Contents: React.FC = () => {
 interface CardProps {
   title: string;
   text: string;
-  tag: string;
+  tag: string[];
   src: string;
 }
 
@@ -40,16 +62,34 @@ const Card: React.FC<CardProps> = ({ title, text, tag, src }) => {
     <div className="card-container">
       <div className="card">
         <div className="card-front" style={{ backgroundImage: `url(${src})` }}>
-          <div className="absolute inset-0 flex flex-col justify-center items-left bg-black/50 rounded-md text-white p-4">
-            <h3 className="text-xl font-bold mb-2">{title}</h3>
-            <p className="text-sm text-left">{text}</p>
+          {/* 카드 전면 : Learn more버튼 하단 고정 */}
+          <div className="absolute inset-0 flex flex-col justify-between bg-black/20 rounded-md text-white p-4">
+            <div>
+              <h3 className="text-xl font-bold mb-2">{title}</h3>
+              <p className="text-sm text-left">{text}</p>
+            </div>
+            <button className="text-stone-50 text-lg py-2 px-6 rounded-full mt-4 hover:bg-stone-300">
+              Learn more
+              <ArrowIcon className="inline-block ml-2" />
+            </button>
           </div>
         </div>
+        {/* 카드 후면: 버튼을 하단에 고정 */}
         <div className="card-back">
-          <div className="flex flex-col justify-center items-center">
-            <h3 className="text-xl font-bold mb-2 text-white">{title}</h3>
-            <p className="text-sm text-scampi-500">{tag}</p>
-            <button className="bg-stone-100 text-neutral-800 text-sm py-2 px-6 rounded-md mt-4 hover:bg-stone-300">콘텐츠 탐색하기</button>
+          <div className="flex flex-col justify-between items-start h-full">
+            <div>
+              <h3 className="text-xl font-bold mb-2 text-white">{title}</h3>
+              {/* 여러 개의 태그를 렌더링 */}
+              <div className="flex flex-wrap justify-items-start space-x-2">
+                {tag.map((t, index) => (
+                  <span key={index} className="text-sm text-scampi-900 bg-stone-50 py-2 px-6 rounded-full mt-4">{t}</span>
+                ))}
+              </div>
+            </div>
+            <button className="text-stone-50 text-lg py-2 px-6 rounded-full mt-4 hover:bg-stone-50 hover:text-scampi-900">
+              콘텐츠 탐색하기
+              <ArrowIcon className="inline-block ml-2" />
+            </button>
           </div>
         </div>
       </div>
--- client2/src/pages/Auth/Mypage.tsx
+++ client2/src/pages/Auth/Mypage.tsx
@@ -3,7 +3,124 @@ import React from 'react';
 import Header from '../../components/Header';
 const Mypage: React.FC = () => {
     return (
+        <>
         <Header />
+        <div className="FootersV5 w-96 h-96 relative bg-white">
+    <div className="FooterDescription w-96 h-24 left-[109.85px] top-[194.40px] absolute text-slate-400 text-lg font-normal font-['DM Sans'] leading-loose">단단한 나를 만드는 5분 글쓰기 습관, 매글! 간편한 <br/>이메일 가입으로 지금 바로 시작해 볼까요? </div>
+    <div className="NewsletterInput w-96 left-[111px] top-[311.74px] absolute">
+        <div className="MasterInputText w-60 h-16 p-6 left-0 top-0 absolute bg-slate-50 rounded-3xl justify-start items-start gap-60 inline-flex">
+            <div className="Container h-5 justify-start items-center gap-2.5 flex">
+                <div className="TextIcons h-5 justify-start items-center gap-60 flex">
+                    <div className="InputContent h-5 justify-start items-start gap-2 flex">
+                        <div className="InputPlaceholder text-center text-slate-400 text-base font-normal font-['DM Sans'] leading-none">Enter your email</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="MasterPrimaryButton w-28 h-14 px-6 py-4 left-[257px] top-[6.54px] absolute bg-slate-500 rounded-3xl justify-start items-start gap-2 inline-flex">
+            <div className="ButtonText text-center text-white text-base font-bold font-['DM Sans'] leading-none">Sign Up</div>
+        </div>
+    </div>
+    <div className="FooterBottom w-96 h-16 left-[103px] top-[431.64px] absolute">
+        <div className="SocialMediaContainer h-9 left-[1028px] top-[26.36px] absolute justify-start items-start gap-4 inline-flex">
+            <div className="SocialMediaIconSquareFacebook w-9 h-9 relative" />
+            <div className="SocialMediaIconSquareInstagram w-9 h-9 relative">
+                <img className="Instagram w-0.5 h-0.5 left-[21.75px] top-[12.01px] absolute" src="https://via.placeholder.com/2x2" />
+            </div>
+            <div className="SocialMediaIconSquareTwitter w-9 h-9 relative" />
+        </div>
+        <div className="Divider w-96 h-px left-[1220px] top-0 absolute origin-top-left rotate-180 border border-gray-300"></div>
+        <div className="Copyright w-96 h-5 left-[6px] top-[34.88px] absolute text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">Copyright © 2024 릿미 | All Rights Reserved</div>
+    </div>
+    <div className="FooterContent w-96 h-80 left-[625.28px] top-[130px] absolute">
+        <div className="FooterSection w-32 h-80 left-0 top-[0.80px] absolute">
+            <div className="FooterTitle w-20 h-5 left-0 top-0 absolute text-slate-500 text-xl font-bold font-['DM Sans'] leading-tight">안내 사항</div>
+            <div className="LinksGroup w-32 h-64 left-0 top-[65.40px] absolute">
+                <div className="Link h-4 left-0 top-0 absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">회사 소개  </div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[39.24px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">제휴 문의 </div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[78.48px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">회원 약관</div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[117.72px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">개인정보처리방침</div>
+                    </div>
+                </div>
+                <div className="Link h-5 left-0 top-[196.20px] absolute" />
+                <div className="Link h-5 left-0 top-[235.44px] absolute" />
+            </div>
+        </div>
+        <div className="FooterSection w-28 h-52 left-[297.72px] top-[1px] absolute">
+            <div className="FooterTitle w-20 h-5 left-0 top-0 absolute text-slate-500 text-xl font-bold font-['DM Sans'] leading-tight">이용 안내</div>
+            <div className="LinksGroup w-28 h-32 left-0 top-[65.40px] absolute">
+                <div className="Link h-4 left-0 top-0 absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">매글 소개</div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[39.24px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">콘텐츠 제휴</div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[78.48px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">매글 비즈니스</div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[117.72px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex" />
+                </div>
+            </div>
+        </div>
+        <div className="FooterSection w-20 h-52 left-[549.72px] top-0 absolute">
+            <div className="FooterTitle w-20 h-5 left-0 top-0 absolute text-slate-500 text-xl font-bold font-['DM Sans'] leading-tight">고객 센터</div>
+            <div className="LinksGroup w-20 h-32 left-0 top-[65.40px] absolute">
+                <div className="Link h-4 left-0 top-0 absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">도움말</div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[39.24px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">공지사항</div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[78.48px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">이메일 문의 </div>
+                    </div>
+                </div>
+                <div className="Link h-4 left-0 top-[117.72px] absolute justify-start items-start inline-flex">
+                    <div className="MasterLink justify-start items-center gap-1.5 flex">
+                        <div className=" text-center text-slate-400 text-lg font-normal font-['DM Sans'] leading-none">챗봇 문의</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="ImageGroup w-40 h-32 left-[496.87px] top-[89.95px] absolute">
+            <div className="ImageIcon w-8 h-5 left-0 top-0 absolute" />
+            <div className="ImageIcon w-8 h-5 left-[132px] top-[102.77px] absolute" />
+            <div className="ImageIcon w-8 h-5 left-0 top-[102.77px] absolute" />
+        </div>
+    </div>
+    <div className="Group51 w-48 left-[111px] top-[114px] absolute">
+        <div className="Group5Copy w-9 h-7 left-0 top-0 absolute">
+        </div>
+        <div className="Typographystyle w-36 h-8 left-[48px] top-0 absolute text-slate-500 text-3xl font-bold font-['DM Sans'] leading-9">MAEGEUL</div>
+    </div>
+</div>
+</>
     );
 }
     export default Mypage;
