e356d94343033a9da08fff5e8da0af32ab713039
Yiyangse <azziyang@naver.com>
2024-09-04 12:30:36 +0900

    fix Contents.css 호버효과수정

--- client2/src/components/Banner.tsx
+++ client2/src/components/Banner.tsx
@@ -1,3 +1,4 @@
+//client2/src/components/Banner.tsx
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
@@ -7,7 +8,7 @@ const Banner: React.FC = () => {
     'https://picsum.photos/1257/743?random=2',
     'https://picsum.photos/1257/743?random=3',
   ];
-  
+
   const [currentSlide, setCurrentSlide] = useState(0);
   const [isPaused, setIsPaused] = useState(false);
 
@@ -22,31 +23,38 @@ const Banner: React.FC = () => {
   }, [isPaused, slides.length]);
 
   return (
-    <div className="relative w-[1257px] h-[743px] bg-neutral-700/40 rounded-3xl overflow-hidden">
-      <div
-        className="relative w-full h-full"
-        onMouseEnter={() => setIsPaused(true)}
-        onMouseLeave={() => setIsPaused(false)}
-      >
+    <div className="flex items-center justify-center min-h-screen"> {/* Flexbox로 가운데 정렬 */}
+      <div className="relative w-[1257px] h-[743px] bg-neutral-700/40 rounded-3xl overflow-hidden justify-center">
         <div
-          className="absolute inset-0 flex transition-transform duration-700"
-          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+          className="relative w-full h-full justify-center"
+          onMouseEnter={() => setIsPaused(true)}
+          onMouseLeave={() => setIsPaused(false)}
         >
-          {slides.map((slide, index) => (
-            <img
-              key={index}
-              src={slide}
-              alt={`Slide ${index + 1}`}
-              className="w-full h-full object-cover"
-            />
-          ))}
-        </div>
-        {/* 고정된 텍스트와 링크 버튼 */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white text-base font-bold font-['Arial']">
-          <h1 className="mb-4 text-2xl">매일 나를 위한 5분 글쓰기 지원 플랫폼</h1>
-          <Link to="/maegeul" className="bg-white text-black py-2 px-4 rounded-full shadow-md hover:bg-gray-200 transition-colors">
-            지금 쓰러가기
-          </Link>
+          <div
+            className="absolute inset-0 flex transition-transform duration-700"
+            style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+          >
+            {slides.map((slide, index) => (
+              <img
+                key={index}
+                src={slide}
+                alt={`Slide ${index + 1}`}
+                className="w-full h-full object-cover"
+              />
+            ))}
+          </div>
+          {/* 고정된 텍스트와 링크 버튼 */}
+          <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white text-base font-bold font-['Arial']">
+            <h1 className="mb-4 text-4xl">매일 나를 위한 5분 <br />글쓰기 지원 플랫폼</h1>
+            <p className="text-center text-scampi-100 dark:text-scampi-600 text-base font-normal uppercase leading-none tracking-wider mb-4">
+              감정일기& AI기록 가이드로 소중한 나의 일상을 가꿔요.<br />더 단단한 나를 만드는 하루 5분 글쓰기, 매글
+            </p>
+            <Link to="/maegeul">
+              <button className="text-sm bg-white text-scampi-700 dark:text-scampi-200 py-2 px-4 rounded-full border border-scampi-50 dark:border-scampi-600 hover:bg-scampi-300 dark:hover:bg-scampi-700 cursor-pointer transition-colors">
+                지금 쓰러가기
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
--- client2/src/components/Navi.tsx
+++ client2/src/components/Navi.tsx
@@ -1,6 +1,5 @@
 // src/components/Navi.tsx
 import React from 'react';
-// SVG 파일을 React 컴포넌트로 임포트
 import { ReactComponent as ArrowIcon } from '../Icon/Arrow Right.svg';
 import { ReactComponent as SearchIcon } from '../Icon/Search.svg';
 import { Link } from 'react-router-dom';
@@ -8,68 +7,80 @@ import { Link } from 'react-router-dom';
 const Navi: React.FC = () => {
   return (
     <div className="relative w-full h-auto p-8 flex flex-col items-center">
-    <div className='grid-rows-2'>
       <h1 className="text-scampi-700 dark:text-scampi-300 text-4xl font-bold font-['DM Sans'] leading-10 mb-12">
         나를 발견하기 위한 공간
       </h1>
-      <div className="flex flex-wrap justify-center gap-12">
-        {/* 첫 번째 카드 */}
-        <Link to="/maegeul" className="group w-96 h-60 bg-scampi-50 dark:bg-scampi-900 rounded-2xl p-8 transition-colors">
-          <p className="text-left text-scampi-400 dark:text-scampi-600 text-base font-normal uppercase leading-none tracking-wider mb-4 group-hover:bg-scampi-100 group-hover:dark:bg-scampi-900">
-            작은 실천으로 시작하는 단단한 나
-          </p>
-          <h2 className="text-left text-scampi-700 dark:text-scampi-300 text-2xl font-bold leading-7 group-hover:bg-scampi-50 group-hover:dark:bg-scampi-900 ">
-            매일 글쓰기
-          </h2>
-        </Link>
-
-        {/* 두 번째 카드 */}
-        <Link to="/emotionForm" className="group w-96 h-60 bg-scampi-50 dark:bg-scampi-900 rounded-2xl p-8 transition-colors">
-          <p className="text-left text-scampi-400 dark:text-scampi-600 text-base font-normal uppercase leading-none tracking-wider mb-4 group-hover:bg-scampi-100 group-hover:dark:bg-scampi-900">
-             AI가 분석해주는 나의 하루
-          </p>
-          <h2 className="text-left text-scampi-700 dark:text-scampi-300 text-2xl font-bold leading-7 group-hover:bg-scampi-50 group-hover:dark:bg-scampi-900 ">
-            AI 하루진단
-          </h2>
-        </Link>
-
-        {/* 세 번째 큰 카드 */}
-        <Link to="/mydiary" className="group w-full h-auto bg-scampi-50 dark:bg-scampi-900 rounded-2xl p-8 flex flex-col justify-between transition-colors">
-          <div>
-          <p className="text-left text-scampi-400 dark:text-scampi-600 text-base font-normal uppercase leading-none tracking-wider mb-4 group-hover:bg-scampi-100 group-hover:dark:bg-scampi-900">
-            나의 일상 & 감정 아카이브
+      <div className="flex justify-center items-start gap-12">
+        {/* 왼쪽: 두 개의 작은 카드 */}
+        <div className="flex flex-col gap-12">
+          {/* 첫 번째 카드 */}
+          <Link
+            to="/maegeul"
+            className="group w-96 h-60 bg-scampi-50 dark:bg-scampi-900 rounded-2xl p-8 transition-colors hover:bg-scampi-100 hover:dark:bg-scampi-800"
+          >
+            <p className="text-left text-scampi-400 dark:text-scampi-600 text-base font-bold uppercase leading-none tracking-wider mb-4">
+              작은 실천으로 시작하는 단단한 나
             </p>
-            <h2 className="text-left text-scampi-700 dark:text-scampi-300 text-2xl font-bold leading-7 group-hover:bg-scampi-50 group-hover:dark:bg-scampi-900 ">
-            마이 다이어리
+            <h2 className="text-left text-scampi-700 dark:text-scampi-300 text-2xl font-bold leading-7">
+              매일 글쓰기
+            </h2>
+          </Link>
+
+          {/* 두 번째 카드 */}
+          <Link
+            to="/emotionForm"
+            className="group w-96 h-60 bg-scampi-50 dark:bg-scampi-900 rounded-2xl p-8 transition-colors hover:bg-scampi-100 hover:dark:bg-scampi-800"
+          >
+            <p className="text-left text-scampi-400 dark:text-scampi-600 text-base font-normal uppercase leading-none tracking-wider mb-4">
+              AI가 분석해주는 나의 하루
+            </p>
+            <h2 className="text-left text-scampi-700 dark:text-scampi-300 text-2xl font-bold leading-7">
+              AI 하루진단
+            </h2>
+          </Link>
+        </div>
+
+        {/* 오른쪽: 큰 카드 */}
+        <Link
+          to="/mydiary"
+          className="group w-[480px] h-full bg-scampi-50 dark:bg-scampi-900 rounded-2xl p-8 flex flex-col justify-between transition-colors hover:bg-scampi-100 hover:dark:bg-scampi-800"
+        >
+          <div>
+            <p className="text-left text-scampi-400 dark:text-scampi-600 text-base font-normal uppercase leading-none tracking-wider mb-4">
+              나의 일상 & 감정 아카이브
+            </p>
+            <h2 className="text-left text-scampi-700 dark:text-scampi-300 text-2xl font-bold leading-7">
+              마이 다이어리
             </h2>
           </div>
           <div className="flex flex-col gap-4">
             {/* 첫 번째 버튼 */}
-            <div className="w-full h-14 bg-transparent border border-scampi-600 rounded-3xl flex items-center px-6 hover:bg-scampi-100 dark:hover:bg-scampi-800 transition-colors cursor-pointer group-hover:border-scampi-600 ">
-              <SearchIcon className="w-4 h-4 mr-2 group-hover:text-scampi-900 group-hover:dark:text-scampi-900" />
-              <span className="text-scampi-700 dark:text-scampi-300 text-lg ">
+            <div className="w-full h-14 bg-transparent border border-scampi-600 rounded-3xl flex items-center px-6 hover:bg-scampi-200 dark:hover:bg-scampi-700 transition-colors cursor-pointer">
+              <SearchIcon className="w-4 h-4 mr-2" />
+              <span className="text-scampi-700 dark:text-scampi-300 text-lg">
                 나의 하루를 단어 구름으로 본다면?
               </span>
-              <ArrowIcon className="w-4 h-4 ml-auto group-hover:text-scampi-50 group-hover:dark:text-scampi-900" />
+              <ArrowIcon className="w-4 h-4 ml-auto" />
             </div>
             {/* 두 번째 버튼 */}
-            <div className="w-full h-14 bg-transparent border border-scampi-600 rounded-3xl flex items-center px-6 hover:bg-scampi-100 dark:hover:bg-scampi-800 transition-colors cursor-pointer group-hover:border-scampi-600">
-              <SearchIcon className="w-4 h-4 mr-2 group-hover:text-scampi-900 group-hover:dark:text-scampi-900" />
-              <span className="text-scampi-700 dark:text-scampi-300 text-lg ">나의 감정 변화 그래프는 어떤 모습일까?
+            <div className="w-full h-14 bg-transparent border border-scampi-600 rounded-3xl flex items-center px-6 hover:bg-scampi-200 dark:hover:bg-scampi-700 transition-colors cursor-pointer">
+              <SearchIcon className="w-4 h-4 mr-2" />
+              <span className="text-scampi-700 dark:text-scampi-300 text-lg">
+                나의 감정 변화 그래프는 어떤 모습일까?
               </span>
-              <ArrowIcon className="w-4 h-4 ml-auto group-hover:text-scampi-50 group-hover:dark:text-scampi-900" />
+              <ArrowIcon className="w-4 h-4 ml-auto" />
             </div>
             {/* 세 번째 버튼 */}
-            <div className="w-full h-14 bg-transparent border border-scampi-600 rounded-3xl flex items-center px-6 hover:bg-scampi-100 dark:hover:bg-scampi-800 transition-colors cursor-pointer group-hover:border-scampi-600">
-              <SearchIcon className="w-4 h-4 mr-2 group-hover:text-scampi-900 group-hover:dark:text-scampi-900" />
-              <span className="text-scampi-700 dark:text-scampi-300 text-lg ">지금 나에게 필요한 콘텐츠는 뭘까?
+            <div className="w-full h-14 bg-transparent border border-scampi-600 rounded-3xl flex items-center px-6 hover:bg-scampi-200 dark:hover:bg-scampi-700 transition-colors cursor-pointer">
+              <SearchIcon className="w-4 h-4 mr-2" />
+              <span className="text-scampi-700 dark:text-scampi-300 text-lg">
+                지금 나에게 필요한 콘텐츠는 뭘까?
               </span>
-              <ArrowIcon className="w-4 h-4 ml-auto group-hover:text-scampi-50 group-hover:dark:text-scampi-900" />
+              <ArrowIcon className="w-4 h-4 ml-auto" />
             </div>
           </div>
         </Link>
       </div>
-    </div>
     </div>
   );
 };
