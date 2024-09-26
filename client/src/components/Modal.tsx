d426432f76cffd9d911f3e8acd0f4a226d516a5f
김영하 <younghotkim@gmail.com>
2024-09-26 12:41:27 +0900

    add modal

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom"; // 회원가입 링크를 사용하기 위해 react-router-dom import
+import mainLogo from "../logo/main_logo.png";
 
 interface ModalProps {
+  isOpen: boolean;
   message: string;
   onClose: () => void;
 }
 
-const Modal: React.FC<ModalProps> = ({ message, onClose }) => {
+const Modal: React.FC<ModalProps> = ({ isOpen, message, onClose }) => {
+  if (!isOpen) return null;
+
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg text-center">
-        <p className="text-lg font-semibold">{message}</p>
-        <button
-          onClick={onClose}
-          className="mt-4 bg-scampi-500 text-white py-2 px-4 rounded shadow hover:bg-scampi-400 transition"
-        >
-          닫기
-        </button>
+      <div className="bg-white p-8 rounded-lg shadow-lg max-w-md w-full text-center font-medium font-plus-jakarta-sans">
+        {/* 로고 크기 조정 */}
+        <img
+          src={mainLogo}
+          className="w-24 h-24 mx-auto mb-10"
+          alt="Main Logo"
+        />
+        {/* 메세지 섹션 */}
+        <p className="text-xl font-bold mb-10">{message}</p>
+
+        <div className="flex justify-around mt-10">
+          {/* 회원가입 버튼 */}
+          <Link to="/mainsignup">
+            <button className="bg-blue-500 text-white text-lg px-6 py-3 rounded-lg hover:bg-purple-600 transition-colors duration-200">
+              회원가입
+            </button>
+          </Link>
+
+          {/* 닫기 버튼 */}
+          <button
+            className="bg-gray-500 text-white text-lg px-6 py-3 rounded-lg hover:bg-gray-600 transition-colors duration-200"
+            onClick={onClose}
+          >
+            닫기
+          </button>
+        </div>
       </div>
     </div>
   );
 };
 
 export default Modal;
+
+export {};
